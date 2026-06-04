14a00573be18f2623675f853e1340e0cf1d275bf
Yossifraslan <raslanyossif@gmail.com>
2026-06-04 17:12:33 +0800

    Implement Homepage UI.

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -46,10 +46,56 @@ export default function Home() {
               <div className="upload-icon">
                 <Layers className="icon" />
               </div>
-
+              <h3>Upload your floor plan</h3>
+              <p>Supports JPG, PNG, formats up to</p>
             </div>
-            <h3>Upload your floor plan</h3>
-            <p>Supports JPG, PNG, formats up to 10MB</p>
+
+            <p>Upload Images</p>
+          </div>
+        </div>
+      </section>
+
+      <section className="projects">
+        <div className="section-inner">
+          <div className="section-head">
+            <div className="copy">
+              <h2>Projects</h2>
+              <p>
+                Your latest work and shared community projects, all in one
+                place.
+              </p>
+            </div>
+          </div>
+
+          <div className="projects-grid">
+            <div className="project-card group">
+              <div className="preview">
+                <img
+                  src="https://roomify-mlhuk267-dfwu1i.puter.site/projects/1770803585402/rendered.png"
+                  alt="Project"
+                />
+
+                <div className="badge">
+                  <span>Community</span>
+                </div>
+              </div>
+
+              <div className="card-body">
+                <div>
+                  <h3>Project Manhattan</h3>
+
+                  <div className="meta">
+                    <Clock size={12} />
+                    <span>{new Date("01.01.2027").toLocaleDateString()}</span>
+                    <span>By SPMK</span>
+                  </div>
+                </div>
+
+                <div className="arrow">
+                  <ArrowUpRight size={18} />
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </section>
