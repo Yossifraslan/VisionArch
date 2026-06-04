e6a07b9a681edc306f11dd9d4d7c2b28a15a4365
Yossifraslan <raslanyossif@gmail.com>
2026-06-04 16:38:03 +0800

    Added Navbar Handling signIn and signOut.

--- componens/Navbar.tsx
+++ componens/Navbar.tsx
@@ -1,55 +1,72 @@
-import { Box } from "lucide-react";
+import {Box} from "lucide-react";
 import Button from "./ui/Button";
+import {useOutletContext} from "react-router";
 
 const Navbar = () => {
-  const isSignedIn = true;
-  const username = "Pakdad";
+    const { isSignedIn, userName, signIn, signOut } = useOutletContext<AuthContext>()
 
-  const handleAuthClick = async () => {};
-  return (
-    <header className="navbar">
-      <nav className="inner">
-        <div className="left">
-          <div className="brand">
-            <Box className="logo" />
+    const handleAuthClick = async () => {
+        if(isSignedIn) {
+            try {
+                await signOut();
+            } catch (e) {
+                console.error(`Puter sign out failed: ${e}`);
+            }
 
-            <span className="name">VisionArch</span>
-          </div>
+            return;
+        }
 
-          <ul className="links">
-            <a href="#">Product</a>
-            <a href="#">Pricing</a>
-            <a href="#">Community</a>
-            <a href="#">Enterprise</a>
-          </ul>
-        </div>
+        try {
+            await signIn();
+        } catch (e) {
+            console.error(`Puter sign in failed: ${e}`);
+        }
+    };
 
-        <div className="actions">
-          {isSignedIn ? (
-            <>
-              <span className="greeting">
-                {username ? `Hi, ${username}` : `Signed in`}
-              </span>
+    return (
+        <header className="navbar">
+            <nav className="inner">
+                <div className="left">
+                    <div className="brand">
+                        <Box  className="logo" />
 
-              <Button size="sm" onClick={handleAuthClick} className="btn">
-                Log Out{" "}
-              </Button>
-            </>
-          ) : (
-            <>
-              <button onClick={handleAuthClick} className="login">
-                Log In
-              </button>
+                        <span className="name">
+                            Roomify
+                        </span>
+                    </div>
 
-              <a href="#upload" className="cta">
-                Get Started
-              </a>
-            </>
-          )}
-        </div>
-      </nav>
-    </header>
-  );
-};
+                    <ul className="links">
+                        <a href="#">Product</a>
+                        <a href="#">Pricing</a>
+                        <a href="#">Community</a>
+                        <a href="#">Enterprise</a>
+                    </ul>
+                </div>
 
-export default Navbar;
+                <div className="actions">
+                    {isSignedIn ? (
+                        <>
+                            <span className="greeting">
+                                {userName ? `Hi, ${userName}` : 'Signed in'}
+                            </span>
+
+                            <Button size="sm" onClick={handleAuthClick} className="btn">
+                                Log Out
+                            </Button>
+                        </>
+                    ) : (
+                        <>
+                            <Button onClick={handleAuthClick} size="sm" variant="ghost">
+                                Log In
+                            </Button>
+
+                            <a href="#upload" className="cta">Get Started</a>
+                        </>
+                    )}
+                </div>
+            </nav>
+        </header>
+    )
+}
+
+export default Navbar
