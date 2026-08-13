a17d3bcf8746682cb93ce290a54e469d79bb13e8
Yossifraslan <raslanyossif@gmail.com>
2026-08-13 11:52:20 +0800

    Add routes for about and privacy pages to the application.

--- app/routes.ts
+++ app/routes.ts
@@ -2,6 +2,8 @@ import { type RouteConfig, index, route } from "@react-router/dev/routes";
 
 export default [
   index("routes/home.tsx"),
+  route("about", "./routes/about.tsx"),
+  route("privacy", "./routes/privacy.tsx"),
   route("visualizer/:id", "./routes/visualizer.$id.tsx"),
   route("community", "./routes/community.tsx"),
   route("draw", "./routes/draw.tsx"),
