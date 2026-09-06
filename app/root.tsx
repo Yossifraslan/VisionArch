c64efe49f25d5ffa3696eda223ceea98691317be
Yossifraslan <raslanyossif@gmail.com>
2026-09-06 11:50:44 +0800

    Update font stylesheet to use DM Sans and DM Serif Display for improved typography

--- app/root.tsx
+++ app/root.tsx
@@ -36,7 +36,7 @@ export const links: Route.LinksFunction = () => [
   },
   {
     rel: "stylesheet",
-    href: "https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap",
+    href: "https://fonts.googleapis.com/css2?family=DM+Sans:wght@400;500;600;700&family=DM+Serif+Display:ital@0;1&display=swap",
   },
 ];
 
